eaf5a42387f3d5266d7995a591e30e1820cf7340
Jakub Maciocha <ciamcio@dellg15jm.home>
2024-12-17 21:40:22 +0100

    Changed top.v. Conflict resolving.

--- hdl/sim_top.v
+++ hdl/sim_top.v
@@ -3,5 +3,6 @@ module sim_top();
 //timer_tb timer_test();
 //presc_tb presc_test();
 spi_tb   spi_test();
+ss_display_tb ss_display_test(); 
 
 endmodule
--- hdl/top.v
+++ hdl/top.v
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 module top
   (
     input btn_0,
@@ -114,6 +115,39 @@ timer     tim0
             .clk_in(sys_clk_pin),
             .sig_out(start_trans)
           );
+=======
+module top(
+  input btn_0,
+  input wire sys_clk_pin,
+  output [3:0] control_out_pins,
+  output [6:0] display_out_pins, 
+  output led_0
+);
+
+
+wire new_data;
+wire [15:0] bcd_reg;
+assign led_0 = new_data;
+
+    
+timer tim0 (
+  .clk_in(sys_clk_pin),
+  .sig_out(new_data)
+);
+
+bcd_register reg0 (
+  .spi_data(16'h1234),
+  .new_data_triger(sys_clk_pin),
+  .bcd_values(bcd_reg)
+);
+
+ss_display dis0  (
+    .bcd_register(bcd_reg),
+    .clk(sys_clk_pin),
+    .control_pins(control_out_pins),
+    .display_pins(display_out_pins)
+);
+>>>>>>> 2158503 (Module implemenatation of seven segment display)
 
 spi       spi0
           (
